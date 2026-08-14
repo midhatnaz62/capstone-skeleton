e9e3a944d2e88767ee6290f741ff1d8d759bcc70
Midhat Naz <midhatnaz62@gmail.com>
2026-08-15 00:00:05 +0500

    Fix Vitest configuration

--- vitest.config.js
+++ vitest.config.js
@@ -2,21 +2,11 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: /\.(js|jsx|ts|tsx)$/,
-    }),
-  ],
-
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.js$/,
-  },
-
+  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: "./vitest.setup.js",
-    include: ["__tests__/**/*.test.{js,jsx}"],
+    include: ["tests/**/*.test.{js,jsx}"],
     globals: true,
   },
 });
